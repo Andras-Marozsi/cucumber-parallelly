42be00e02b11c1015429a38acc43245268b3d068
Andras Marozsi <andras@grata.com>
2023-03-16 15:51:16 -0400

    Update cucumber

--- src/cucumber-parallelly/helper.js
+++ src/cucumber-parallelly/helper.js
@@ -1,14 +1,13 @@
 "use strict";
 
-var
-  fs = require('fs'),
-  pathHelper = require('path'),
-  pathSeparator = pathHelper.sep;
+const fs = require('fs')
+const pathHelper = require('path')
+const pathSeparator = pathHelper.sep
 
-const { PickleFilter, getTestCasesFromFilesystem } = require('cucumber');
+const { PickleFilter, getTestCasesFromFilesystem } = require('@cucumber/cucumber');
 const { EventEmitter } = require('events');
 const eventBroadcaster = new EventEmitter();
-
+const { GherkinStreams } = require('@cucumber/gherkin-streams');
 /**
  * Helper class containing some util functions
  */
@@ -19,7 +18,7 @@ class Helper {
    * @param path The path of the report file for the given scenario
    * @returns {Number} Returns the retry count for the scenario
    */
-  static getRetryCountFromPath (path) {
+  static getRetryCountFromPath(path) {
     return parseInt(path.replace('.json', '').substr(path.length - 6))
   }
 
@@ -27,7 +26,7 @@ class Helper {
    * Create the folder structure for the provided path
    * @param completePath The complete path of the file (can be just the folder structure as well)
    */
-  static createFolderStructure (completePath) {
+  static createFolderStructure(completePath) {
     var path = '';
     completePath = pathHelper.normalize(completePath);
     completePath.split(pathSeparator).map(function (folder) {
@@ -51,7 +50,7 @@ class Helper {
    * @param pathToReport The path to the report file to examine
    * @returns {boolean} True if there were undefined steps, false otherwise
    */
-  static hadUndefinedStep (pathToReport) {
+  static hadUndefinedStep(pathToReport) {
     var actualReport = JSON.parse(fs.readFileSync(pathToReport, 'utf8'))[0],
       hasUndefinedStep = false;
 
@@ -67,7 +66,7 @@ class Helper {
   /**
    * Collects all scenarios to 'arrayOfScenarioPaths' variable, sorted by some special tags if they were provided
    */
-  static getScenarios (tagsToGet, weightingTags, fPaths = ['./features']) {
+  static getScenarios(tagsToGet, weightingTags, fPaths = ['./features']) {
     var
       allScenarios = [],
       sortedScenarios = [],
@@ -78,7 +77,7 @@ class Helper {
       loadFeatureFilesFromDir(path);
     });
 
-    function loadFeatureFilesFromDir (path) {
+    function loadFeatureFilesFromDir(path) {
       fs.readdirSync(path).forEach(file => {
         if (fs.lstatSync(pathHelper.resolve(process.cwd(), path, file)).isDirectory()) {
           loadFeatureFilesFromDir(pathHelper.resolve(process.cwd(), path, file))
@@ -109,26 +108,55 @@ class Helper {
       tagExpression = tagsToGet.tags;
     }
 
-    function getFeaturesByTagExpression () {
-      return getTestCasesFromFilesystem({
-        cwd: '',
-        eventBroadcaster: eventBroadcaster,
-        featurePaths: featuresPaths,
-        pickleFilter: new PickleFilter({
-          tagExpression: tagExpression
-        })
-      }).then(function (results) {
+    function getFeaturesByTagExpression() {
+      if (featuresPaths.length === 0) {
+        return Promise.resolve([])
+      }
+
+      return streamToArray(GherkinStreams.fromPaths(featuresPaths, {
+        includeSource: false,
+        includeGherkinDocument: true,
+        includePickles: true
+      })).then(function (featureFiles) {
+        let gherkinDocument = null
+        let results = []
+        let gherkinDocumentLocations = []
+        let locationsIndexHelper = 0
+        const pickleFilter = (() => new (require('@cucumber/cucumber/lib/pickle_filter')).default({ tagExpression: tagExpression }))();
+
+        featureFiles.forEach(element => {
+          if (element.gherkinDocument) {
+            gherkinDocument = element.gherkinDocument
+            gherkinDocument.feature.children.forEach((scenario) => {
+              if (scenario.scenario.examples.length > 0) {
+                scenario.scenario.examples.forEach((example) => {
+                  example.tableBody.forEach(tableRow => {
+                    gherkinDocumentLocations.push(gherkinDocument.uri + ':' + tableRow.location.line)
+                  })
+                })
+              } else {
+                gherkinDocumentLocations.push(gherkinDocument.uri + ':' + scenario.scenario.location.line)
+              }
+            })
+          } else if (element.pickle && gherkinDocument) {
+            const pickle = element.pickle
+            if (pickleFilter.matches({ gherkinDocument, pickle })) {
+              pickle.customUri = gherkinDocumentLocations[locationsIndexHelper]
+              results.push({ pickle })
+            }
+            locationsIndexHelper++
+          }
+        });
         results.forEach(function (scenario) {
           allScenarios.push(scenario);
         });
-
         // Sort the scenarios by their size (can be defines by tag '@duration_'
         allScenarios.sort(function (scenario1, scenario2) {
-          return getScenarioSize(scenario2.pickle) - getScenarioSize(scenario1.pickle)
+          return getScenarioSize(scenario2.pickle) - getScenarioSize(scenario1.pickle);
         });
         // Create the array of (sorted) scenarios to execute
         allScenarios.map(function (scenario) {
-          sortedScenarios.push(pathHelper.resolve(scenario.uri + ':' + scenario.pickle.locations[0].line));
+          sortedScenarios.push(scenario.pickle.customUri);
         });
         return sortedScenarios;
       }, function (ex) {
@@ -137,7 +165,7 @@ class Helper {
       });
     }
 
-    function getScenarioSize (scenario) {
+    function getScenarioSize(scenario) {
       var size = weightingTags.default;
       scenario.tags.forEach(function (tag) {
         if (tag.name.indexOf(weightingTags.pattern) > -1) {
@@ -147,6 +175,13 @@ class Helper {
       return size;
     }
 
+    const streamToArray = async (readableStream) => new Promise((resolve, reject) => {
+      const items = [];
+      readableStream.on('data', (item) => items.push(item));
+      readableStream.on('error', (err) => reject(err));
+      readableStream.on('end', () => resolve(items));
+    });
+
     return getFeaturesByTagExpression()
   }
 
@@ -154,7 +189,7 @@ class Helper {
    * Set the environment variables got in parameter if they are not yet set
    * @param varsToSet {object} Key value pair of environment variables and their values to set.
    */
-  static setEnvironmentVariables (varsToSet) {
+  static setEnvironmentVariables(varsToSet) {
     if (varsToSet != undefined) {
       Object.keys(varsToSet).map(function (varName) {
         process.env[varName] = process.env[varName] || varsToSet[varName];
